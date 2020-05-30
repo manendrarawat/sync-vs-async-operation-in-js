33b59e513ff86bc773b10b91171d9a35c4525545
manendrarawat <mannuatnri.itm@gmail.com>
2020-05-30 16:35:12 +0530

    2 files modified

--- index.js
+++ index.js
@@ -11,23 +11,36 @@
 // }
 
 // func1();
+const checkButtonClick = () => {
+  console.log('button clicked..');
+
+    let time1 = new Date().getTime();
+    console.log('time1 : ', time1);
+    while(new Date().getTime() - time1 < 10000){
+      
+    }
+}
+
+
+document.getElementById('myButton').addEventListener('click', checkButtonClick);
 
 
 
-/// asynchronnous code execution example
-const func2 = () => {
 
-    setTimeout(() => {
-       console.log('fun2 started..');
-    }, 5000)
+// /// asynchronnous code execution example
+// const func2 = () => {
+
+//     setTimeout(() => {
+//        console.log('fun2 started..');
+//     }, 5000)
    
-}
+// }
 
-const func1 = () => {
-        console.log('fun1 started..');
-          func2();
-        console.log('fun1 ended..');
-}
+// const func1 = () => {
+//         console.log('fun1 started..');
+//           func2();
+//         console.log('fun1 ended..');
+// }
 
-func1();
+// func1();
 
